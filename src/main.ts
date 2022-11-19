6151a06a0f0e3941fdfbf9c6660f15f339dacc27
Rafael Feiten <rafael.feiten@paipe.co>
2022-11-19 13:38:28 -0300

    config hbs on nest and method override

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 import { AppModule } from './app.module';
+import * as hbs from 'hbs';
+import * as method from 'method-override';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('hbs');
+
+  hbs.registerPartials(join(__dirname, '..', 'views/partials'));
+
+  app.use(method('_method'));
+
   await app.listen(3000);
 }
 bootstrap();
